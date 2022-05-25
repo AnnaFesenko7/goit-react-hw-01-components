17bfd377436321f2a6420061edaca0f7c947b393
AnnaFesenko7 <annafecenko7@gmail.com>
2022-05-25 12:59:24 +0200

    remove files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,16 @@
-import Profile from './Profile/profile';
-import Statistics from 'components/Statistics/statistics';
-import FriendList from './FriendList/friendList';
-import TransactionHistory from './Transaction/transactionHistory';
-import user from 'components/Profile/user.json';
-import data from 'components/Statistics/data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
-
 export const App = () => {
   return (
-    <div>
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Profile
-          userName={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-      </div>
-
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Statistics stats={data} />
-      </div>
-
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <FriendList friends={friends} />
-      </div>
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 18,
-          color: '#010101',
-        }}
-      >
-        <TransactionHistory items={transactions} />
-      </div>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -8,22 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-
-// const image = React.createElement('img', {
-//   src: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
-//   alt: 'Tacos With Lime',
-//   width: 640,
-// });
-// const title = React.createElement('h2', null, 'Tacos With Lime');
-// const price = React.createElement('p', null, 'Price: 10.99$');
-// const button = React.createElement('button', { type: 'button' }, 'Add to cart');
-
-// const product = React.createElement('div', null, image, title, price, button);
-
-/*
- * Для передачі дітей також використовується властивість children параметра props.
- * Зверніть увагу на те, що властивість children – це масив.
- */
-// const productWithChildrenInProps = React.createElement('div', {
-//   children: [image, title, price, button],
-// });
